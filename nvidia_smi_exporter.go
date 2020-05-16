acae2e38888477580a280b9b0d1060966a198f6f
Glyn Owen Hanmer <glynhanmer@gmail.com>
2020-05-16 17:24:22 +0100

    ✨ add more fields to query gpu args

--- nvidia_smi_exporter.go
+++ nvidia_smi_exporter.go
@@ -39,23 +39,7 @@ func main() {
 
 func metrics(logger log.Logger) func(http.ResponseWriter, *http.Request) {
 	command := envOrDefault("NVIDIA_SMI", "nvidia-smi")
-	fields := []string{
-		"driver_version",
-		"temperature.gpu",
-		"utilization.gpu",
-		"utilization.memory",
-		"memory.total",
-		"memory.free",
-		"memory.used",
-		"fan.speed",
-		"power.draw",
-		"clocks.current.graphics",
-		"clocks.current.sm",
-		"clocks.current.memory",
-		"clocks.current.video",
-		"encoder.stats.sessionCount",
-		"encoder.stats.averageFps",
-		"encoder.stats.averageLatency"}
+	fields := defaultFields()
 	metricList := make([]string, len(fields))
 	for i, field := range fields {
 		metricList[i] = strings.Replace(field, ".", "_", -1)
@@ -111,6 +95,84 @@ func metrics(logger log.Logger) func(http.ResponseWriter, *http.Request) {
 			}
 		}
 	}
+}
+
+func defaultFields() []string {
+	return []string{
+		"clocks.applications.gr",
+		"clocks.applications.graphics",
+		"clocks.applications.mem",
+		"clocks.applications.memory",
+		"clocks.current.graphics",
+		"clocks.current.memory",
+		"clocks.current.sm",
+		"clocks.current.video",
+		"clocks.default_applications.gr",
+		"clocks.default_applications.graphics",
+		"clocks.default_applications.mem",
+		"clocks.default_applications.memory",
+		"clocks.gr",
+		"clocks.max.gr",
+		"clocks.max.graphics",
+		"clocks.max.mem",
+		"clocks.max.memory",
+		"clocks.max.sm",
+		"clocks.mem",
+		"clocks.sm",
+		"clocks_throttle_reasons.gpu_idle",
+		"clocks_throttle_reasons.hw_power_brake_slowdown",
+		"clocks_throttle_reasons.hw_slowdown",
+		"clocks_throttle_reasons.hw_thermal_slowdown",
+		"clocks_throttle_reasons.sw_thermal_slowdown",
+		"clocks_throttle_reasons.sync_boost",
+		"clocks.video",
+		"driver_version",
+		"ecc.errors.corrected.aggregate.device_memory",
+		"ecc.errors.corrected.aggregate.l1_cache",
+		"ecc.errors.corrected.aggregate.l2_cache",
+		"ecc.errors.corrected.aggregate.register_file",
+		"ecc.errors.corrected.aggregate.texture_memory",
+		"ecc.errors.corrected.aggregate.total",
+		"ecc.errors.corrected.volatile.l1_cache",
+		"ecc.errors.corrected.volatile.l2_cache",
+		"ecc.errors.corrected.volatile.register_file",
+		"ecc.errors.corrected.volatile.texture_memory",
+		"ecc.errors.corrected.volatile.total",
+		"ecc.errors.uncorrected.aggregate.device_memory",
+		"ecc.errors.uncorrected.aggregate.l1_cache",
+		"ecc.errors.uncorrected.aggregate.l2_cache",
+		"ecc.errors.uncorrected.aggregate.register_file",
+		"ecc.errors.uncorrected.aggregate.texture_memory",
+		"ecc.errors.uncorrected.aggregate.total",
+		"ecc.errors.uncorrected.volatile.device_memory",
+		"ecc.errors.uncorrected.volatile.l1_cache",
+		"ecc.errors.uncorrected.volatile.l2_cache",
+		"ecc.errors.uncorrected.volatile.register_file",
+		"ecc.errors.uncorrected.volatile.texture_memory",
+		"ecc.errors.uncorrected.volatile.total",
+		"encoder.stats.averageFps",
+		"encoder.stats.averageLatency",
+		"encoder.stats.sessionCount",
+		"enforced.power.limit",
+		"fan.speed",
+		"memory.free",
+		"memory.total",
+		"memory.used",
+		"power.default_limit",
+		"power.draw",
+		"power.limit",
+		"power.management",
+		"power.max_limit",
+		"power.min_limit",
+		"pstate",
+		"retired_pages.dbe",
+		"retired_pages.double_bit.count",
+		"retired_pages.pending",
+		"retired_pages.sbe",
+		"retired_pages.single_bit_ecc.count",
+		"temperature.gpu",
+		"utilization.gpu",
+		"utilization.memory"}
 }
 
 func envOrDefault(key string, defaultValue string) string {
